62eb95de7c5b1592c08ef1286c530e15eb50c9bd
Stas Khvan <hvanstas@gmail.com>
2023-11-08 12:53:12 +0300

    таск 4

---
+++ src/modules/searchHints/searchHints.ts
@@ -0,0 +1,8 @@
+import html from './searchHints.tpl.html';
+import { Component } from '../component';
+
+class SearchHints extends Component {
+
+}
+
+export const searchHints = new SearchHints(html);
--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import { notFoundComp } from './modules/notFound/notFound';
 import { homepageComp } from './modules/homepage/homepage';
 import { productDetailComp } from './modules/productDetail/productDetail';
 import { checkoutComp } from './modules/checkout/checkout';
+import { searchHints } from './modules/searchHints/searchHints';
 
 const ROUTES = {
   '/': homepageComp,
   '/catalog': catalogComp,
   '/product': productDetailComp,
-  '/checkout': checkoutComp
+  '/checkout': checkoutComp,
+  '/search': searchHints
 };
 
 export default class Router {
